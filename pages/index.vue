c72248e3bb3238bbf1fbf40ffa028898f34e9a40
bhoomikhurana <96761198+bhoomikhurana@users.noreply.github.com>
2023-08-18 17:00:35 +0530

    added API

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+           
+  <h2>Home</h2>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis impedit dolorem eligendi error iste asperiores perferendis itaque obcaecati praesentium libero?</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+h2{
+    margin-bottom:20px;
+    font-size:36px;
+}
+p{
+    margin:20px 0;
+}
+</style>
